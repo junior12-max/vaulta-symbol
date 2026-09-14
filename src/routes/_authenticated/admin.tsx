8481c8c3d38928d924491cbb4e3e519e48fc3a5b
junior12-max <solomonashiegbu007@gmail.com>
2026-09-14 13:41:41 +0100

    Update admin.tsx

--- src/routes/_authenticated/admin.tsx
+++ src/routes/_authenticated/admin.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { useServerFn } from "@tanstack/react-start";
+import { useServerFn } from "@tanstack/react-router";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
 
