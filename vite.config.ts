05380caef7ee8a5d0870b03aa6527fef2cc25b64
junior12-max <solomonashiegbu007@gmail.com>
2026-09-14 13:24:35 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,6 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-  ],
-  build: {
-    cssMinify: 'esbuild',
-  },
+  plugins: [react(), tsconfigPaths()],
   appType: 'spa',
 })
