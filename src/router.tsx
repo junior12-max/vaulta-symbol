b38df17570dd965ca3d49f38a8a312dd4b79a242
junior12-max <solomonashiegbu007@gmail.com>
2026-09-14 10:47:42 +0100

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -2,27 +2,19 @@ import { QueryClient } from '@tanstack/react-query'
 import { createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
-export const getRouter = () => {
-  const queryClient = new QueryClient()
+const queryClient = new QueryClient()
 
-  const router = createRouter({
-    routeTree,
-    context: {
-      queryClient,
-    },
-    scrollRestoration: true,
-    defaultPreloadStaleTime: 0,
-  })
-
-  return router
-}
-
-const router = getRouter()
+export const router = createRouter({
+  routeTree,
+  context: {
+    queryClient,
+  },
+  scrollRestoration: true,
+  defaultPreloadStaleTime: 0,
+})
 
 declare module '@tanstack/react-router' {
   interface Register {
     router: typeof router
   }
 }
-
-export { router }
