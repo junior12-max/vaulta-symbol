62384f95e30810c45ba34347a11287e33625f306
junior12-max <solomonashiegbu007@gmail.com>
2026-09-17 07:56:21 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
-import { router } from './router'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { router, queryClient } from './router'
 import './styles.css'
 
 const rootElement = document.getElementById('root')!
@@ -10,7 +11,9 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </React.StrictMode>
   )
 }
