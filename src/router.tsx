4a3f8537bfa442ef948e8323acc3c0b397e2292c
junior12-max <solomonashiegbu007@gmail.com>
2026-09-17 07:54:32 +0100

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -2,7 +2,7 @@ import { QueryClient } from '@tanstack/react-query'
 import { createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
-const queryClient = new QueryClient()
+export const queryClient = new QueryClient()
 
 export const router = createRouter({
   routeTree,
