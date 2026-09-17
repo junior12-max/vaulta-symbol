1de1704d95bf903376d68117ef1cd7de100970d5
junior12-max <solomonashiegbu007@gmail.com>
2026-09-17 07:34:52 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  build: {
-    cssMinify: false,
-  },
-  appType: 'spa',
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths()
+  ],
+  appType: 'spa'
 })
