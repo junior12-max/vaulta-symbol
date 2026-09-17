970297555b94b0ba78347d25300eba427cba0609
junior12-max <solomonashiegbu007@gmail.com>
2026-09-17 08:54:10 +0100

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 import type { QueryClient } from '@tanstack/react-query'
+import { SplashScreen } from '../components/splash-screen'
 
 interface MyRouterContext {
   queryClient: QueryClient
@@ -10,5 +11,10 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 })
 
 function RootComponent() {
-  return <Outlet />
+  return (
+    <>
+      <SplashScreen />
+      <Outlet />
+    </>
+  )
 }
